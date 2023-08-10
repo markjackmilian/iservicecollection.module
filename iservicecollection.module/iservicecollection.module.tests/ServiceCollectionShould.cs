b6030887dbb30a80c5da0d472a148f3cc5249bde
Marco Milani <emme.milani@hotmail.it>
2023-08-10 11:37:09 +0200

    tests: add tests

---
+++ iservicecollection.module/iservicecollection.module.tests/ServiceCollectionShould.cs
@@ -0,0 +1,57 @@
+
+using System.Reflection;
+using iservicecollection.module.core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace iservicecollection.module.tests;
+
+public class ServiceCollectionShould
+{
+    [Fact]
+    public void AddModuleUsingGeneric()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddModule<TestModule>();
+        
+        Assert.Contains(serviceCollection, a => a.ServiceType == typeof(ITest));
+    }
+    
+    [Fact]
+    public void AddModuleUsingInstance()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddModule(new TestModule());
+        
+        Assert.Contains(serviceCollection, a => a.ServiceType == typeof(ITest));
+    }
+    
+    [Fact]
+    public void AddModuleUsingAssemblyScan()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.ScanModules(typeof(ServiceCollectionShould).Assembly);
+        
+        Assert.Contains(serviceCollection, a => a.ServiceType == typeof(ITest));
+    }
+    
+   
+}
+
+
+class TestModule : IModule
+{
+    public void Load(IServiceCollection services)
+    {
+        services.AddTransient<ITest, Test>();
+    }
+}
+
+interface ITest
+{
+    string Name { get; }
+}
+
+class Test : ITest
+{
+    public string Name => "Test";
+}
